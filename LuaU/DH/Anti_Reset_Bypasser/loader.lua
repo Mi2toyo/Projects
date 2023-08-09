3b3906776a2b17d302d3f6bc7e9957bdff5b9286
Mi2toyo <116574573+Mi2toyo@users.noreply.github.com>
2023-08-08 22:25:11 -0700

    Update loader.lua

--- LuaU/DH/Anti_Reset_Bypasser/loader.lua
+++ LuaU/DH/Anti_Reset_Bypasser/loader.lua
@@ -1,2 +1,6 @@
+--[[
+  Basically prevents you from losing your guns when resetting below 90% health
+]]
+
 getgenv().Anti_Reset_Bypasser = true
 loadstring(game:HttpGet'https://raw.githubusercontent.com/Mi2toyo/Projects/main/LuaU/DH/Anti_Reset_Bypasser/src.lua')()
