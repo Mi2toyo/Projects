041c46ba50e5e85584d61ca3a65f2940d107711d
Mi2toyo <116574573+Mi2toyo@users.noreply.github.com>
2023-08-09 20:07:04 -0700

    Update src.lua

--- LuaU/DH/Bring_Kick/src.lua
+++ LuaU/DH/Bring_Kick/src.lua
@@ -1,6 +1,11 @@
+repeat
+    task.wait()
+until game:IsLoaded()
 local function findService(service)
     return game:FindService(service)
 end
+findService'Players'.localPlayer.Character:WaitForChild'FULLY_LOADED_CHAR'
+
 while getgenv().Bring_Kick do
     if findService'Players'.localPlayer.Character.BodyEffects.Grabbed.Value ~= nil and findService'Players':FindFirstChild(tostring(findService'Players'.localPlayer.Character.BodyEffects.Grabbed.Value)) then
         findService'Players'.localPlayer.Character.BodyEffects.Grabbed.Value.LeftUpperLeg.Postion = Vector3.new(100, -1200, 100)
