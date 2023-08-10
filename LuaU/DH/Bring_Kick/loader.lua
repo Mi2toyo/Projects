4129c48570faabd8e6631b3026030aa290fe5ce0
Mi2toyo <116574573+Mi2toyo@users.noreply.github.com>
2023-08-09 19:57:29 -0700

    Update loader.lua

--- LuaU/DH/Bring_Kick/loader.lua
+++ LuaU/DH/Bring_Kick/loader.lua
@@ -1,2 +1,7 @@
+--[[
+  Kicks everyone you ragdoll-brings
+  Unbring to kick
+]]
+
 getgenv().Bring_Kick = true
 loadstring(game:HttpGet'https://raw.githubusercontent.com/Mi2toyo/Projects/main/LuaU/DH/Bring_Kick/src.lua')()
