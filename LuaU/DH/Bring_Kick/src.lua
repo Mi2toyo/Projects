c996cc282b2adc83f0e0f5540b605da3e848b581
Mi2toyo <116574573+Mi2toyo@users.noreply.github.com>
2023-08-09 20:16:24 -0700

    Update src.lua

--- LuaU/DH/Bring_Kick/src.lua
+++ LuaU/DH/Bring_Kick/src.lua
@@ -7,10 +7,13 @@ end
 findService'Players'.localPlayer.Character:WaitForChild'FULLY_LOADED_CHAR'
 
 while getgenv().Bring_Kick do
-    if findService'Players'.localPlayer.Character.BodyEffects.Grabbed.Value ~= nil and findService'Players':FindFirstChild(tostring(findService'Players'.localPlayer.Character.BodyEffects.Grabbed.Value)) then
-        findService'Players'.localPlayer.Character.BodyEffects.Grabbed.Value.LeftUpperLeg.Postion = Vector3.new(100, -1200, 100)
-        repeat
-            task.wait()
-        until findService'Players'.localPlayer.Character.BodyEffects.Grabbed.Value == nil
-    end
+    pcall(function()
+        if findService'Players'.localPlayer.Character.BodyEffects.Grabbed.Value ~= nil and findService'Players':FindFirstChild(tostring(findService'Players'.localPlayer.Character.BodyEffects.Grabbed.Value)) then
+            findService'Players'.localPlayer.Character.BodyEffects.Grabbed.Value.LeftUpperLeg.Postion = Vector3.new(100, -1200, 100)
+            repeat
+                task.wait()
+            until findService'Players'.localPlayer.Character.BodyEffects.Grabbed.Value == nil
+        end
+    end)
+    task.wait()
 end
