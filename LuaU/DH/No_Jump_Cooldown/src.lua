03cc93cee124aa8894d7923296216fdd5db1b0f4
Mi2toyo <116574573+Mi2toyo@users.noreply.github.com>
2023-08-09 20:08:01 -0700

    Update src.lua

--- LuaU/DH/No_Jump_Cooldown/src.lua
+++ LuaU/DH/No_Jump_Cooldown/src.lua
@@ -1,6 +1,12 @@
--- getgenv().No_Jump_Cooldown = true
+repeat
+    task.wait()
+until game:IsLoaded()
+local function findService(service)
+    return game:FindService(service)
+end
+findService'Players'.localPlayer.Character:WaitForChild'FULLY_LOADED_CHAR'
 
-for _, connection in pairs(getconnections(game:FindService'Players'.localPlayer.Character:FindFirstChildWhichIsA'Humanoid'.Jumping)) do
+for _, connection in pairs(getconnections(findService'Players'.localPlayer.Character:FindFirstChildWhichIsA'Humanoid'.Jumping)) do
     if getinfo(connection).name == '' then
         connection:Disable()
     end
