cd1ccc6d14fcb10ecf5f7b5bd7aa3eee542c74f2
Mi2toyo <116574573+Mi2toyo@users.noreply.github.com>
2023-08-29 06:02:58 -0700

    Update src.lua

--- LuaU/DH/No_Jump_Cooldown/src.lua
+++ LuaU/DH/No_Jump_Cooldown/src.lua
@@ -8,6 +8,10 @@ findService'Players'.localPlayer.Character:WaitForChild'FULLY_LOADED_CHAR'
 
 for _, connection in pairs(getconnections(findService'Players'.localPlayer.Character:FindFirstChildWhichIsA'Humanoid'.Jumping)) do
     if getinfo(connection).name == '' then
-        connection:Disable()
+        if getgenv.No_Jump_Cooldown then
+            connection:Disable()
+        else
+            connection:Disable()
+        end
     end
 end
