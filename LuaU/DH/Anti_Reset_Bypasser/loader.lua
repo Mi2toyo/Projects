58b6d21b8227ecebf382047cfc847346c06073b8
Mi2toyo <116574573+Mi2toyo@users.noreply.github.com>
2023-08-30 02:51:23 -0700

    Delete LuaU/DH/Anti_Reset_Bypasser directory

--- LuaU/DH/Anti_Reset_Bypasser/loader.lua
+++
@@ -1,6 +0,0 @@
---[[
-  Basically prevents you from losing your guns when resetting below 90% health
-]]
-
-getgenv().Anti_Reset_Bypasser = true
-loadstring(game:HttpGet'https://raw.githubusercontent.com/Mi2toyo/Projects/main/LuaU/DH/Anti_Reset_Bypasser/src.lua')()
