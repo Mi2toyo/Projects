2770b34c2c2cb96915819e2cf6f082a6efb96a70
Mi2toyo <116574573+Mi2toyo@users.noreply.github.com>
2023-08-30 02:56:49 -0700

    Update loader.lua

---
+++ LuaU/DH/Reset_Plus/loader.lua
@@ -0,0 +1,4 @@
+--// No more losing your guns when reset below 100% health
+getgenv().Reset_Plus = true
+
+loadstring(game:HttpGet'https://raw.githubusercontent.com/Mi2toyo/Projects/main/LuaU/DH/Reset_Plus/src.lua')()
