98f5ea42716443b9f330ed5b63a9fad2dca6b371
Mi2toyo <116574573+Mi2toyo@users.noreply.github.com>
2023-09-02 03:21:18 -0700

    Delete LuaU/DH/No_Jump_Cooldown/loader.lua

--- LuaU/DH/No_Jump_Cooldown/loader.lua
+++
@@ -1,4 +0,0 @@
---// Removes DH's Jump Cooldown
-getgenv().No_Jump_Cooldown = true
-
-loadstring(game:HttpGet'https://raw.githubusercontent.com/Mi2toyo/Projects/main/LuaU/DH/No_Jump_Cooldown/src.lua')()
